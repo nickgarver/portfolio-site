9f58d425a38a5410f6cd468843db86d328c95660
Nick Garver <nickgarver5@gmail.com>
2024-10-02 17:00:14 -0400

    Update about.js

--- components/about.js
+++ components/about.js
@@ -10,14 +10,16 @@ export default function About(){
           <h2 className="pressheader">Hello!</h2>
         </div>
         <p className="pressbio" >
-        I'm Nick, an IT graduate at the University of Cincinnati.
+        Hello! I am Nick, an IT graduate at the University of Cincinnati.
         I am always seeking new opportunities to learn, collaborate,
         and explore. Currently I have been doing freelance web design
-        and working for 'Melody App' to stay up to date with current practices.
-         Feel free to reach out with any questions about
+        and working for 'Hivemind' to stay up to date with current practices. 
+        Living in Detroit I also do part time cultivation for a Cannabis company; 
+        I am always learing about the ever growing cannabis industry.
+        Feel free to reach out with any questions about
         my work, experiences, or just to chat.
         </p>
-        <a className="resume" href="https://drive.google.com/file/d/1kZ09maI29D8Hrctr45s-6Z4iKt8wv35v/view?usp=sharing">
+        <a className="resume" href="https://docs.google.com/document/d/1Q4aZ7jC4-XTd0r3ZLu2nIEbHr-Tnk9KlSV8k3lPXeOw/edit?usp=sharing">
           <FontAwesomeIcon className="resumeicn" icon={faFileAlt} />
           <span className="resumetxt">Resume</span>
         </a>
